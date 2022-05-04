db25fd31126aa19801fc7091f971aa36796d7381
PeterPottR <62199244+PeterPottR@users.noreply.github.com>
2022-05-05 00:25:57 +0200

    LoadLevel implemented

--- SurviveTheExam/SurviveTheExam/Logic/GameLogic.cs
+++ SurviveTheExam/SurviveTheExam/Logic/GameLogic.cs
@@ -1,6 +1,7 @@
 ﻿using SurviveTheExam.Repository;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,34 @@ namespace SurviveTheExam.Logic
 {
     public class GameLogic : ILogic
     {
+        public IGameModel.MapItem[,] Map
+        { get; set; }
+
+        private Queue<string> levelNames =
+            new Queue<string>();
+
+
+
+
         //private readonly IModel model;
         private readonly IRepository repo;
 
         //ctor ahol this.model=model és this.repo=repo
+        public GameLogic()
+        {
+            var lvls = Directory.GetFiles(Path
+        .Combine(Directory
+        .GetCurrentDirectory(),
+        "Levels"));
+
+            foreach (string lvl in lvls)
+            {
+                levelNames.Enqueue(lvl);
+            }
+
+            LoadLevel(levelNames.Dequeue());
+        }
+
 
         public event EventHandler GameOver;
 
@@ -33,6 +58,57 @@ namespace SurviveTheExam.Logic
 
         //??refreshscreen?
 
+        private void LoadLevel(string fileName)
+        {
 
+            string[] lines =
+                File.ReadAllLines(fileName);
+
+            Map = new IGameModel.MapItem[
+                int.Parse(lines[0]),
+                int.Parse(lines[1])];
+
+            for (int x = 0; x < Map.GetLength(0); x++)
+            {
+                for (int y = 0; y < Map.GetLength(1); y++)
+                {
+                    Map[x, y] = CharToMapItem(lines[y + 2][x]);
+                    if (Map[x, y] == IGameModel.MapItem.Player)
+                    {
+                        playerX = x;
+                        playerY = y;
+                    }
+                }
+            }
+
+            // TODO: jatekos kezdeti
+            // poziciojanak beallitasa
+        }
+
+        private IGameModel.MapItem
+            CharToMapItem(char c)
+        {
+
+            switch (c)
+            {
+                case '0': return IGameModel.MapItem.Wall;
+                case '1': return IGameModel.MapItem.Wall;
+                case '2': return IGameModel.MapItem.Wall;
+                case '3': return IGameModel.MapItem.Wall;
+                case '4': return IGameModel.MapItem.Wall;
+                case '5': return IGameModel.MapItem.Wall;
+                case '6': return IGameModel.MapItem.Wall;
+                case '7': return IGameModel.MapItem.Wall;
+                case '8': return IGameModel.MapItem.Wall;
+                case 's': return IGameModel.MapItem.Player;
+                case 'f': return IGameModel.MapItem.Exit;
+                case 'o': return IGameModel.MapItem.Otos;
+                case 'z': return IGameModel.MapItem.Zh;
+                default: return IGameModel.MapItem.Floor;
+            }
+        }
     }
+
+
+}
 }
