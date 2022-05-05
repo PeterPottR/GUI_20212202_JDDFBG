596137093b84373fb66f5b10b949d584be204250
Noncsi <noncsi2249@gmail.com>
2022-05-05 18:51:48 +0200

    Model created

---
+++ SurviveTheExam/SurviveTheExam/Model/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SurviveTheExam.Model
+{
+    public class Player : Element
+    {
+        public Player(int x, int y)
+        {
+            this.area = new Rect(x, y, Config.PlayerWidth, Config.PlayerHeight);
+        }
+    }
+}
