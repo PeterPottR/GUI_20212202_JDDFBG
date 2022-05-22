f53a32acb56493cb710085ffce7909651ea1418a
Bodrogi Lili <bodrogilili@stud.uni-obuda.hu>
2022-05-22 21:07:32 +0200

    Score class

--- SurviveTheExam/SurviveTheExam/Model/Score.cs
+++ SurviveTheExam/SurviveTheExam/Model/Score.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace SurviveTheExam.Model
 {
     public class Score : Element
     {
-        public Score(double x, double y)
+        public Score(double x, double y, List<Tuple<int, int, int>> where)
         {
-            this.area = new Rect(x, y, Config.ScoreTextSize, Config.ScoreTextSize);
+            this.area = new Rect(x, y, 49, 44);
+            where.Add(Tuple.Create(int.Parse((this.area.X + 24).ToString()), int.Parse((this.area.Y + 22).ToString()), 1));
         }
+        public Score()
+        {
+
+        }
+
+        public int ScoreNum { get; set; }
+
+        public BitmapImage pic = new BitmapImage(new Uri(Path.Combine("images", "score.png"), UriKind.RelativeOrAbsolute));
+
     }
 }
